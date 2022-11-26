752076b5b5aff86c201ed9de4ac30ff8cb9d52b8
Bartosz Kazuła <bartosz@kazula.eu>
2022-11-27 00:11:25 +0100

    Added required footer for printing

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import { SkillsetCard } from "../components/SkillsetCard";
 
 const IndexPage = ({ data }) => {
     return (
-        <main className="flex justify-around p-5 print:justify-start print:p-0 xl:container xl:mx-auto">
+        <>
             <aside className="basis-4/12 print:mr-2 print:basis-5/12">
                 <AboutCard />
                 <SkillsetCard />
@@ -23,6 +23,18 @@ const IndexPage = ({ data }) => {
                 </Card>
             </article>
         </main>
+            </main>
+            <footer className="clear-both mt-11 hidden print:block">
+                <p className="text-xs">
+                    I agree to the processing of personal data provided in this document for realising the recruitment
+                    process pursuant to the Personal Data Protection Act of 10 May 2018 (Journal of Laws 2018, item
+                    1000) and in agreement with Regulation (EU) 2016/679 of the European Parliament and of the Council
+                    of 27 April 2016 on the protection of natural persons with regard to the processing of personal data
+                    and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection
+                    Regulation).
+                </p>
+            </footer>
+        </>
     );
 };
 
